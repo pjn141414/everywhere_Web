0c06f17c82ac215a3739d11f832f8b59d8ddce61
pjn141414 <jina071049@gmail.com>
2022-06-20 17:23:23 +0900

    Add: color

--- src/styles/color.tsx
+++ src/styles/color.tsx
@@ -1,8 +1,9 @@
-const color = {
+const colors = {
   background: '#FFFFFF',
   primaryText: '#000000',
   secondaryText: '#767676',
   buttonBackground: '#E3E6ED',
+  primary: '#EE7C5A',
 }
 
-export default color;
+export default colors;
