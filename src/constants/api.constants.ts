3c99b93e8de8457915c5b9eb3ddc0af0cc7a58d3
pjn141414 <jina071049@gmail.com>
2022-06-20 17:23:01 +0900

    Add: constants

---
+++ src/constants/api.constants.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_KEY = 'access_token' as const;
+export const REFRESH_TOKEN_KEY = 'refresh_token' as const;
+export const TOKEN_HEADER_KEY = 'Authorization' as const;
+export const ALLOW_CORS_KEY = 'Access-Control-Allow-Origin' as const;
